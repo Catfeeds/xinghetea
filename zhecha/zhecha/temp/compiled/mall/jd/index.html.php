2a75a9ffc04fc93d108dbab48034ac3f06b18ea5
WANG <wang1521107147@163.com>
2018-01-15 21:35:12 +0800

    y

--- zhecha/zhecha/temp/compiled/mall/jd/index.html.php
+++ zhecha/zhecha/temp/compiled/mall/jd/index.html.php
@@ -17,10 +17,121 @@
     <div class="col-3 w" area="col-3" widget_type="area">
       <?php $this->display_widgets(array('page'=>'index','area'=>'col-3')); ?>
     </div>
-      <div style="width: 200px;height: 200px;background: red"></div>
+
+      <div class="j1">
+        <h1>阶梯一</h1>
+        <div class="j1_center j11">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+        <div class="j1_center">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+        <div class="j1_center">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+      </div>
+
+    <div class="j2">
+      <h1>阶梯二</h1>
+      <div class="j2_center j22">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+      <div class="j2_center">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+    </div>
+
+    <div class="j3">
+      <h1>阶梯三</h1>
+      <div class="j3_center">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+    </div>
+
+    <div class="j4">
+      <h1>阶梯四</h1>
+      <div class="j4_center">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+    </div>
+
+    <div class="j5">
+      <h1>阶梯五</h1>
+      <div class="j5_center1">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+      <div class="j5_center2">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+    </div>
+
+    <div class="j6">
+      <h1>阶梯六</h1>
+      <div class="j6_center1">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+      <div class="j6_center2">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+    </div>
+
+    <div class="j7">
+      <h1>阶梯七</h1>
+      <div class="j7_left">
+
+        <div class="j7_left_center j77">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+
+        <div class="j7_left_center">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+
+        <div class="j7_left_center j77">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+
+        <div class="j7_left_center">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+
+      </div>
+
+      <div class="j7_right">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+
+    </div>
+
+    <div class="j8">
+      <h1>阶梯八</h1>
+      <div class="j8_left">
+        <img src="../../../data/files/mall/template/201709261555394284.jpg">
+      </div>
+      <div class="j8_right">
+        <div class="j8_right_center j88">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+
+        <div class="j8_right_center">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+
+        <div class="j8_right_center j88">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+
+        <div class="j8_right_center">
+          <img src="../../../data/files/mall/template/201709261555394284.jpg">
+        </div>
+      </div>
+    </div>
+
+
+
   </div>
   <?php if ($this->_var['index']): ?>
   <div class="J_FloorNav floor-nav"></div>
-  <?php endif; ?> 
+  <?php endif; ?>
 </div>
-<?php echo $this->fetch('footer.html'); ?> 
+<?php echo $this->fetch('footer.html'); ?>
